bbab0cdc3609e19ebe5342d09b3e7d10dc23ee37
ysantonyance <zinovevakarina569@gmail.com>
2026-06-21 22:58:52 +0300

    Hi from eniamon

--- Program.cs
+++ Program.cs
@@ -122,6 +122,9 @@ namespace CatShelter
             var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
             app.Run($"http://0.0.0.0:{port}");
 
+
+            // Hi from eniamon
+
         }
     }
 }
